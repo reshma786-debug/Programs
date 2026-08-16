e97362d74bc9a9dfc5396feaf5770bb6f598d381
reshma786-debug <reshmarattu786@gmail.com>
2026-08-16 20:44:02 +0530

    Update EmptyTraingle.java

--- src/SDET/EmptyTraingle.java
+++ src/SDET/EmptyTraingle.java
@@ -10,7 +10,6 @@ public static void main(String[] args) {
             }
             // Stars and spaces inside
             for (int j = 1; j <= 2 * i - 1; j++) {
-
                 if (i == n || j == 1 || j == 2 * i - 1) {
                     System.out.print("*");
                 } else {
