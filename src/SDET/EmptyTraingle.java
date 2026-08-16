ba3b95d5b7be6e16c6aec8583aadf66fe4679b85
reshma786-debug <reshmarattu786@gmail.com>
2026-08-16 20:43:44 +0530

    Update EmptyTraingle.java

--- src/SDET/EmptyTraingle.java
+++ src/SDET/EmptyTraingle.java
@@ -1,20 +1,23 @@
 package SDET;
 
 public class EmptyTraingle {
+public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // Spaces before *
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Stars and spaces inside
+            for (int j = 1; j <= 2 * i - 1; j++) {
 
-	public static void main(String arg[]) {
-		int num = 6;
-
-		for (int i = 1; i <= num; i++) {
-			for (int j = 1; j <= i-1; j++) {
-				System.out.print(" ");
-			}
-
-			for (int k = 1; k <=i*2-1; k++) {
-				System.out.print("j");
-			}
-			
-			System.out.println("");
-		}
-	}
+                if (i == n || j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
